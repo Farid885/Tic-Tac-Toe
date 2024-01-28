5c08e78c0adec4dfd7f081e2d6fe49a72646a987
Farid885 <ferid14zamanli@gmail.com>
2024-01-28 18:42:26 +0400

    some commence

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
+  function handleSelect(){
+console.log('sa')
+  }
   return (
-    <div>
-      
-    </div>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player   initialName="Player 2" symbol="O" />
+        </ol>
+        Game Board
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    console.log(isEditing);
+  }
+
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+   
+  }
+
+  return (
+    <>
+      <li>
+        <span className="player">
+          {isEditing ? (
+            <input type="text" required value={playerName} onChange={handleChange} />
+          ) : (
+            <span className="player-name"> {playerName} </span>
+          )}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}> {isEditing ? "Save" : "Edit"} </button>
+      </li>
+    </>
+  );
+}
+
+export default Player;
